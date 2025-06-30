c0a86070c5e712206442da87498241e7ce9537e8
andrearcaina <dtandre331@gmail.com>
2025-06-30 10:29:35 -0400

    user input

---
+++ basics/user_input.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Andre on 2025-06-30.
+//
+#include <iostream>
+
+int main() {
+    int x;
+
+    std::cout << "Type a number: ";
+    std::cin >> x;
+    std::cout << "Your number is: " << x;
+
+    return 0;
+}
